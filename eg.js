0e855201c5b205567a530f77e33f215e3a7607ab
vikassingh5522 <vikas.singh66551@gmail.com>
2025-12-15 22:37:08 +0530

    time --> 10 : 36

--- eg.js
+++ eg.js
@@ -1,4 +1,10 @@
 
 function exampleFunction() {
     console.log("This is an example function.");
+
+    console.log("Additional functionality can be added here."); 
+
+    console.log("End of the example function.");
+    
 }
+exampleFunction();
